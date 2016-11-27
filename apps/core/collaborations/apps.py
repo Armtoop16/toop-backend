500c7d9eb72e45b16a84f84c83e3aceabd0b13d6
araratpoghosyan <angola-please-disgorge>
2016-11-27 19:38:07 +0400

    [FEAT]: add `collaborations` app

---
+++ apps/core/collaborations/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CollaborationsConfig(AppConfig):
+    name = 'apps.core.collaborations'
---
+++ apps/core/collaborations/models.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Core Django imports
+from django.db import models
+
+
+class Collaboration(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
