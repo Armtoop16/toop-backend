bd409f6899ea42ca5e07a46a2c18a7915c759699
araratpoghosyan <angola-please-disgorge>
2016-11-29 18:14:33 +0400

    [FIX]: add `companies` fields

--- apps/core/companies/models.py
+++ apps/core/companies/models.py
@@ -2,18 +2,31 @@
 
 # Core Django imports
 from django.db import models
+from django.conf import settings
+
+# Local apps imports
+from apps.core.images.models import Image
 
 
 class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ForeignKey(Image)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Moderator(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    company = models.ForeignKey(Company, related_name='moderators')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.FloatField()
+    company = models.ForeignKey(Company, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
