8780bdf0cc3a1e443a80960010c905dd071d337e
araratpoghosyan <angola-please-disgorge>
2016-11-29 18:06:09 +0400

    [FIX]: add name and description to gift

--- apps/core/gifts/models.py
+++ apps/core/gifts/models.py
@@ -5,5 +5,7 @@ from django.db import models
 
 
 class Gift(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
