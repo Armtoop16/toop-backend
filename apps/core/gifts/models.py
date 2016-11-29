7b8aa483470d6ee3c8128f4ae312896dd8336f72
araratpoghosyan <angola-please-disgorge>
2016-11-29 18:10:12 +0400

    [FIX]: add list to gifts

--- apps/core/gifts/models.py
+++ apps/core/gifts/models.py
@@ -3,9 +3,13 @@
 # Core Django imports
 from django.db import models
 
+# Local apps imports
+from apps.core.lists.models import Lists
+
 
 class Gift(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    list = models.ForeignKey(Lists, related_name='gifts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
