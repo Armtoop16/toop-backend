aa1a82b2cd905536bdeacf62a6d13f84a00e4f44
araratpoghosyan <angola-please-disgorge>
2016-12-10 14:10:21 +0400

    [FIX]: add new migrations

--- apps/core/connections/migrations/0002_connection_social_account.py
+++ apps/core/connections/migrations/0002_connection_social_account.py
@@ -9,7 +9,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('socialaccount', '0004_auto_20161107_1627'),
         ('connections', '0001_initial'),
     ]
 
