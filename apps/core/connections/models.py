7fa41ae7f4dde7b2c473359fae031b38087c144b
araratpoghosyan <angola-please-disgorge>
2016-12-10 14:08:16 +0400

    [FIX]: add new migrations

--- apps/core/connections/models.py
+++ apps/core/connections/models.py
@@ -10,5 +10,6 @@ from allauth.socialaccount.models import SocialAccount
 
 class Connection(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='connections', on_delete=models.CASCADE)
+    social_account = models.ForeignKey(SocialAccount)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
